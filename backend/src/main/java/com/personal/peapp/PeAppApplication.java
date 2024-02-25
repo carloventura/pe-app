b9da00aa5e30ceab7044e8e51e660ffd4ca68be4
carlo <carlo@LGwebOSTV.station>
2024-02-25 18:48:09 +0100

    backend setup

---
+++ backend/src/main/java/com/personal/peapp/PeAppApplication.java
@@ -0,0 +1,13 @@
+package com.personal.peaap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.personal.peaap.PeAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/personal/peapp/PeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.peaap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
